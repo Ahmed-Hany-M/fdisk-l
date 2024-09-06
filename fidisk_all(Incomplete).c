4b638f23d423857da336998f3101ff9c7b295b72
Ahmed-Hany-M <ahmedhany6750@gmail.com>
2024-09-06 12:08:15 +0300

    Update fidisk_all(Incomplete).c

--- fidisk_all(Incomplete).c
+++ fidisk_all(Incomplete).c
@@ -8,138 +8,141 @@
 #include <inttypes.h>
 #include <stdbool.h>
 
-typedef struct {
-	uint8_t status;
-	uint8_t first_chs[3];
-	uint8_t partition_type;
-	uint8_t last_chs[3];
-	uint32_t lba;
-	uint32_t sector_count;
-}PartitionEntry;
-
-typedef struct {									//Same as PartitionEntry.
-	uint8_t status;
-	uint8_t first_chs[3];
-	uint8_t partition_type;
-	uint8_t last_chs[3];
-	uint32_t lba;
-	uint32_t sector_count;
-}EBR_entry;
-
-typedef struct EBR_table{
-	uint32_t lba;
-	uint32_t sector_count;
-	EBR_entry partitions[4];						
-	struct EBR_table* next;
-}EBR_table;
-
-void add_EBR_table(EBR_table** head, EBR_table* new_ebr) {
-	if(*head == NULL)
-		*head = new_ebr;
-	else {
-		EBR_table* current = *head;
-		while(current->next != NULL)
-			current = current->next;
-		
-		current->next = new_ebr;
-	}
-}
-	//I will use this for debugging only (for now).
-void print_EBR_list(EBR_table* head) {
-    EBR_table* current = head;
-    while (current != NULL) {
-        printf("EBR Start LBA: %u, Sector Count: %u\n", current->lba, current->sector_count);
-        for (int i = 0; i < 4; ++i) {
-            EBR_entry* entry = &current->partitions[i];
-            if (entry->sector_count > 0) {
-                printf("  Logical Partition %d - Start LBA: %u, Sector Count: %u\n",
-                       i + 1, entry->lba, entry->sector_count);
-            }
-        }
-        current = current->next;
-    }
-}
+struct PartitionEntry {
+    uint8_t status;
+    uint8_t first_chs[3];
+    uint8_t partition_type;
+    uint8_t last_chs[3];
+    uint32_t lba;
+    uint32_t sector_count;
+};
+/*
+struct EBR {
+    uint8_t boot_region[446];
+    struct PartitionEntry first_entry;              
+    struct PartitionEntry second_entry;
+    struct PartitionEntry third_entry;              //Typically Empty.
+    struct PartitionEntry fourth_entry;             //Typically Empty.
+    uint8_t magic_region[2];       
+};
+*/
 
 int main(int argc, char** argv) {
-	char buffer[512];								//Will be used to read MBR.
-	
-	if(argc < 2) {									
-		char* err_msg = "Too few arguments.\n";
-		write(2, err_msg, strlen(err_msg));
-		exit(EXIT_FAILURE);
-	}
-	
-	int fd = open(argv[1], O_RDONLY);
-	if(fd == -1) {									//If file failed to be opened.									
-		char* err_msg = "Unable to open the file.\n";
-		write(2, err_msg, strlen(err_msg));
-		exit(EXIT_FAILURE);
-	}
-	
-	ssize_t read_bytes = read(fd, buffer, sizeof(buffer));
-	if(read_bytes == -1) {								//If reading from the file failed.
-		char* err_msg = "Unable to read.\n";
-		write(2, err_msg, strlen(err_msg));
-		exit(EXIT_FAILURE);
-	}
+    if(argc < 2) {                                  /* Ensuring that the users enters at least 2 arguments.*/
+        char* err_msg = "Missing Argument(s).\n";
+        write(2, err_msg, strlen(err_msg));
+        exit(EXIT_FAILURE);
+    }
 
-	PartitionEntry* table_entry_ptr = (PartitionEntry*)&buffer[446];		//The first 446 bytes are for IPL {Iniital Program Loader}.
-	EBR_table* EBR_list_head = NULL;
-	
-	printf("%-13s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n", "Device", "Boot", "Start", "End", "Sector", "Size", "Id", "Type");
-	for(int i = 0 ; i < 4 ; i++) {
-		if(table_entry_ptr[i].sector_count == 0)
-			continue;
-			
-		printf("%s%-5d %-10c %-10u %-10u %-10u %.1fG       %-10X\n", argv[1], i+1, table_entry_ptr[i].status == 0x80 ? '*' : ' ',
+    char primary_buffer[512];               
+
+    int fd = open(argv[1], O_RDONLY);
+    if(fd == -1) {
+        char* err_msg = "Opening the given path failed.\n";
+        write(2, err_msg, strlen(err_msg));
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t read_primary_bytes = read(fd, primary_buffer, sizeof(primary_buffer));
+    if(read_primary_bytes == -1) {
+        char* err_msg = "Reading from the MBR failed.\n";
+        write(2, err_msg, strlen(err_msg));
+        exit(EXIT_FAILURE);
+    }
+
+    struct PartitionEntry* table_entry_ptr = (struct PartitionEntry*)&primary_buffer[446];
+
+    /* These will be used to determine which partition is the extended one.*/
+    bool is_extended = false;
+    int count = 0;
+    int extended_partition_index = -1;
+
+    printf("%-13s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n", "Device", "Boot", "Start", "End", "Sector", "Size", "Id", "Type");
+
+    for(int i = 0 ; i < 4 ; i++) {
+        if(table_entry_ptr[i].sector_count == 0)
+            continue;
+        
+        if(table_entry_ptr[i].partition_type == 0x05 || table_entry_ptr[i].partition_type == 0x0f) {
+            is_extended = true;
+            count++;
+            extended_partition_index = i;
+        }
+
+        if(count > 1) {
+            char* err_msg = "This has more than one extended partition, hence not MBR format.\n";
+            write(2, err_msg, strlen(err_msg));
+            exit(EXIT_FAILURE);
+        }
+        
+        printf("%s%-5d %-10c %-10u %-10u %-10u %.1fG       %-10X\n", argv[1], i+1, table_entry_ptr[i].status == 0x80 ? '*' : ' ',
 			table_entry_ptr[i].lba, table_entry_ptr[i].lba + table_entry_ptr[i].sector_count - 1, table_entry_ptr[i].sector_count,
 			(double)table_entry_ptr[i].sector_count*512/(1024*1024*1024), table_entry_ptr[i].partition_type);
-		if(table_entry_ptr[i].partition_type == 0x5 ) {
-			uint32_t ebr_lba = table_entry_ptr[i].lba;
-			while(ebr_lba != 0) {
-				EBR_table* new_ebr = (EBR_table*)malloc(sizeof(EBR_table));
-				if(new_ebr == NULL) {
-					char* err_msg = "Memory allocation failed.\n";
-					write(2, err_msg, strlen(err_msg));
-					close(fd);
-					exit(EXIT_FAILURE);
-				}
-				
-				off_t check_lseek = lseek(fd, ebr_lba*512, SEEK_SET);
-				if(check_lseek == -1) {
-					char* err_msg = "lseek failed.\n";
-					write(2, err_msg, strlen(err_msg));
-					close(fd);
-					exit(EXIT_FAILURE);
-				}
-				
-				char bufferEBR[512];
-				ssize_t read_ebr_bytes = read(fd, bufferEBR, sizeof(bufferEBR));
-				if(read_ebr_bytes == -1) {
-					char* err_msg = "reading failed.\n";
-					write(2, err_msg, strlen(err_msg));
-					close(fd);
-					exit(EXIT_FAILURE);
-				}
-				
-				memcpy(new_ebr, bufferEBR, sizeof(EBR_table));
-				new_ebr->lba = ebr_lba;
-				new_ebr->next = NULL;
-				
-				add_EBR_table(&EBR_list_head, new_ebr);
-				/* Moving to the next EBR, which should be present in the first partition of the current EBR.*/
-				ebr_lba = new_ebr->partitions[0].lba;
-				if(ebr_lba == 0)
-					break;
-				
-			}
-		}
-	}
-	print_EBR_list(EBR_list_head);			//For debugging (I will replace it later).
-	
-	
-	
-close(fd);
-return 0;
-}
+    
+    }
 
+    /* In the above, we printed the primary partitions; now we shall focus on the extended partition.*/
+    close(fd);
+    
+    if(is_extended) {
+        int ext_fd = open(argv[1], O_RDONLY);
+        if(ext_fd == -1) {
+            char* err_msg = "Opening the given path failed.\n";
+            write(2, err_msg, strlen(err_msg));
+            exit(EXIT_FAILURE);
+        }
+        off_t to_extended_lba_bytes = table_entry_ptr[extended_partition_index].lba*512;
+        long unsigned int to_extended_lba = table_entry_ptr[extended_partition_index].lba;
+
+        off_t check_lseek = lseek(ext_fd, to_extended_lba_bytes, SEEK_SET);
+        if(check_lseek == -1) {
+            char* err_msg = "lseek failed.\n";
+            write(2, err_msg, strlen(err_msg));
+            exit(EXIT_FAILURE);
+        }
+        
+        char ext_buff[512];
+        ssize_t read_ext_bytes = read(ext_fd, ext_buff, 512);
+        if(read_ext_bytes == -1) {
+            char* err_msg = "Reading EBR failed.\n";
+            write(2, err_msg, strlen(err_msg));
+            exit(EXIT_FAILURE);
+        }
+
+        struct PartitionEntry* EBR_ptr = (struct PartitionEntry*)&ext_buff[446];
+
+        /* First Entry of this EBR:*/
+        long unsigned int rel_lba_first_entry = EBR_ptr->lba;
+        long unsigned int no_sectors_first_entry = EBR_ptr->sector_count;
+
+        //Start and end calculations
+        long unsigned int absolute_start_first_ebr_lba = rel_lba_first_entry + to_extended_lba;
+        long unsigned int absolute_end_first_ebr_lba = absolute_start_first_ebr_lba + no_sectors_first_entry - 1;
+        printf("\n FIRST LOGICAL PARTITION INFO:\n");
+        printf("Start: %lu ; End: %lu ; Sectors: %lu", absolute_start_first_ebr_lba, absolute_end_first_ebr_lba, no_sectors_first_entry);
+
+        /*Now Let's get the next EBR and logical partition info:*/
+        EBR_ptr++;
+        
+        long unsigned int rel_addr_next_EBR = EBR_ptr->lba;
+        /* Let's go to the next EBR and read its data:*/
+        int fdd = open(argv[1], O_RDONLY);
+        long unsigned int abs_lba_next_ebr = table_entry_ptr[extended_partition_index].lba + rel_addr_next_EBR;
+        off_t snd_offset = abs_lba_next_ebr*512;
+        lseek(fdd, snd_offset, SEEK_SET);
+        char ext2_buff[512];
+        read(fdd, ext2_buff, 512);
+        struct PartitionEntry* ptr2 = (struct PartitionEntry*)&ext2_buff[446];
+        long unsigned int rel_lba_first_entry2 = ptr2->lba;
+
+        long unsigned int exact_sectors = EBR_ptr->sector_count - rel_lba_first_entry2;
+        printf("\nSECTORS: %lu\n", exact_sectors);
+        long unsigned int START_2nd_log = ptr2->lba + abs_lba_next_ebr;
+        printf("START: %lu\n", START_2nd_log);
+        printf("END: %lu\n", START_2nd_log + exact_sectors -1);
+    }
+    
+
+    return 0;
+}
+    
